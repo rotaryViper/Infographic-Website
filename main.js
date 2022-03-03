89d385419d542ad14597011eb3ca18bb7338d737
rotaryViper <93423444+rotaryViper@users.noreply.github.com>
2022-03-03 19:14:41 +1100

    This is our backup

---
+++ main.js
@@ -0,0 +1,19 @@
+/*
+Purely for testing, and its too hard lol to fix this code lol
+- Brandon
+
+"use strict"
+
+var res = screen.width;
+function contentPosition(cssClass, width, height, top, left){
+    let Class = document.getElementById(cssClass);
+    Class.style.position = 'absolute';
+    Class.style.width = width + 'px';
+    Class.style.height = height + 'px';
+    Class.style.top = top + 'rem';
+    Class.style.left = left + 'px';
+}
+
+contentPosition('infographic', res*0.55, (res*0.55)*2.5, 3, 0)
+contentPosition('rightBar', res*0.45, (res*0.55)*2.5, 3, res*0.55)
+*/
